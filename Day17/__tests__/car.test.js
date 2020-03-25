0dda966f76d615a0701a4635fe9d7ad01153b11b
Askie Personal GitHub <isme408@gmail.com>
2020-03-25 15:50:04 +0800

    [Day17-mock] feat: 使用spyOn測試真實邏輯

--- Day17/__tests__/car.test.js
+++ Day17/__tests__/car.test.js
@@ -3,6 +3,9 @@ import car from '../src/utils/car';
 
 jest.mock('uuid/v1');
 
+// 先把資料 spy 起來，測試真實邏輯
+const getCurrentCarSpy = jest.spyOn(car, 'getCurrentCar');
+
 describe('addProdToCar', () => {
   // 作用域中使用 beforeAll 生命週期
   // mockReturnValueOnce() 第一次呼叫時塞值
@@ -13,15 +16,22 @@ describe('addProdToCar', () => {
       .mockReturnValueOnce('123');
   });
 
-  test('check_execute_uuid', () => {
+  test('check_add_prod', () => {
     const newCar = car.addProdToCar('apple', 3);
-    // ! 看這裡 console 的結果
-    console.log(newCar);
 
     // 斷言是否有執行過
     // 沒有的話代表 uuid 依賴有問題
-    // 與以下內容要測的事情一樣： 執行次數為一
+    // 與以下內容要測的事情一樣： 執行次數為 1
     // expect(uuid.mock.calls.length).toBe(1);
     expect(uuid).toHaveBeenCalled();
+
+    // 驗證 spy 的真實邏輯有被呼叫
+    expect(getCurrentCarSpy).toHaveBeenCalled();
+
+    // 驗證新車加入時有沒有相等維以下內容
+    // id 是 123 ，是透過上方 mockReturnValueOnce() 塞進來的
+    expect(newCar).toEqual(
+      [{ id: '123', name: 'apple', count: 3 }],
+    );
   });
 });
